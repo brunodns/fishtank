3631146ddaa5d0b86a6ad8a8c650f42329d73ea9
brunodns <92168926+brunodns@users.noreply.github.com>
2021-10-08 17:08:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const GET_FISH_TOKEN = "ZWgsFJJiQnJPdZXnKorQZ8/yE0kxc5P1THHGQmR7oQoNCUmLqnchvOC8CDbJvval/h+Ahsr+OSw="
-const CLAIM_TOKEN = "ZWgsFJJiQnJPdZXnKoqEYJnxFkpkcLSnTXWbEGFY/VkLDTjcqXVx6rPLDDXGwo3wqG6Eh7j+OX1vkmjg+6G9kaWj/dFSGtxL+nvw8SoaF0k0PCDOtU+njw8FNCA="
+const CLAIM_TOKEN = "ZWgsFJJiQnJPdZXnKorQZ8/yE0kxc5P1THHGQmR7oQoNCUmLqnchvOC8CDbJvval/h+Ahsr+OX1vkmjg+6G9kaWj/dFRSdBKoH34oysfRklhb3Kc4x+g21pRNCA="
 const FEED_TOKEN= "ZWgsFJJiQnJPdZXnKorQZ8/yE0kxc5P1THHGQmR7oQoNCUmLqnchvOC8CDbJvval/h+Ahsr+OX1vkmjg+6G9kaWj/dFRSdBKoH34oysfRklhb3Kc4x+g21pRNHFxuiTHGkJP8kjMEsY=";
 
 const axios = require('axios')
