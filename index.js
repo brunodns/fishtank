cfdce154cea95774713a80903fcfa3e379aa1dd3
brunodns <92168926+brunodns@users.noreply.github.com>
2021-10-11 22:02:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function startup() {
     //   'Accept-Language': 'en'
     // }});
 
-const {data} = await axios.post("https://apius.reqbin.com/api/v1/requests",  {"id":"0","name":"","errors":"","json":"{\"method\":\"POST\",\"url\":\"https://api.fishytank.io/rpc\",\"apiNode\":\"US\",\"contentType\":\"JSON\",\"content\":\"{\\\"name\\\":\\\"get_fish_in_tank\\\",\\\"data\\\":\\\"ZWgsFJJiQnJPdZXnKoqMMe2DM0o4e+epTFWaZTMpqwwLXDmMpyMh4rfBeWKTw/eirxmB8br1OSw=\\\"}\",\"headers\":\"\",\"errors\":\"\",\"curlCmd\":\"\",\"codeCmd\":\"\",\"auth\":{\"auth\":\"noAuth\",\"bearerToken\":\"\",\"basicUsername\":\"\",\"basicPassword\":\"\",\"customHeader\":\"\",\"encrypted\":\"\"},\"compare\":false,\"idnUrl\":\"https://api.fishytank.io/rpc\"}","deviceId":"b95a04cc-7065-49f9-9e9f-6a63e11e690eR"}, {
+const {data} = await axios.post("https://apius.reqbin.com/api/v1/requests",  {"id":"0","name":"","errors":"","json":"{\"method\":\"POST\",\"url\":\"https://api.fishytank.io/rpc\",\"apiNode\":\"US\",\"contentType\":\"JSON\",\"content\":\"{\\\"name\\\":\\\"get_fish_in_tank\\\",\\\"data\\\":\\\"ZWgsFJJiQnJPdZXnKorQZ8/yE0kxc5P1THHGQmR7oQoNCUmLqnchvOC8CDbJvval/h+Ahsr+OSw=\\\"}\",\"headers\":\"\",\"errors\":\"\",\"curlCmd\":\"\",\"codeCmd\":\"\",\"auth\":{\"auth\":\"noAuth\",\"bearerToken\":\"\",\"basicUsername\":\"\",\"basicPassword\":\"\",\"customHeader\":\"\",\"encrypted\":\"\"},\"compare\":false,\"idnUrl\":\"https://api.fishytank.io/rpc\"}","deviceId":"b95a04cc-7065-49f9-9e9f-6a63e11e690eR"}, {
       headers:{
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 OPR/79.0.4143.73",
       'Accept-Language': 'en'
